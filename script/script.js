c7468e9e0ba0d3b8a923641cf17f88abec99a08a
Tatiane <weitzeltati@gmail.com>
2021-03-31 22:40:17 -0300

    "Nova pagina e erro da barra de navegacao sem resolver

--- script/script.js
+++ script/script.js
@@ -56,6 +56,8 @@ $('ion-icon#twitter').click(function(){
 
 
 /*animations*/ 
+
+AOS.init();
 /*
 $('section#perfect-sunday').waypoint(function(direction){
     if(direction === "down"){
@@ -80,4 +82,63 @@ $('section#perfect-sunday').waypoint(function(direction){
     })
 */
 
-    
+   $(window).on('scroll',function(){
+       if($(window).scrollTop()){
+           $('nav').addClass('green');
+           $('nav div#logo').addClass('static');
+       }
+       else{
+        $('nav').removeClass('green');
+        $('nav div#logo').removeClass('static');
+       }
+   })
+
+   //Login
+
+
+   const form = document.getElementById('form')
+   const userName = document.getElementById('username')
+   const email = document.getElementById('email')
+   const password = document.getElementById('password')
+   const passwordCheck = document.getElementById('password_check')
+
+
+   form.addEventListener('submit',e =>{
+       e.preventDefault();
+
+
+       checkInputs();
+   });
+
+   function checkInputs(){
+       //pegar valores dos inputs
+   const userNameValue = userName.value.trim();
+   const emailValue = email.value.trim();
+   const passwordValue = password.value.trim();
+   const passwordCheckValue = passwordCheck.value.trim();
+
+   if(userNameValue === " "){
+       //mostrar erro
+       //add error class
+        setErrorFor(userName,"Preencher campo!")
+   }else{
+       //add success class
+       setSuccessFor(userName)
+    }
+}
+
+function setErrorFor(input,message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    // add class
+    formControl.className = "form-control error"
+    // add menssagem de erro
+    small.innerText = message;
+
+
+
+}
+
+function setSuccessFor(){
+
+}
